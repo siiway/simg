f70111a1b3ed51809b3a16dd3cf785967b9bb5bb
wyf9 <wyf9@wyf9.top>
2024-03-02 21:26:22 +0800

    Auto Upload: [2024-03-02 21:26:22]

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # simg
 
-___ Image Download and Hosting *(not r18)*
+___ Image Download and Hosting *(disabled r18)*
 
-**Usage / 使用**: python3 dl.py `count` `last` `r18`
+**Usage / 使用**: python3 dl.py `count` `last` `r18` `auto_upload`
 
 - `const`: 起始数字 (int)
 - `last`: 结束数字 (int)
 - `r18`: *是否获取 r18 图片* **(别开!!!)** (`true` / `false`)
 
 > 是文本, 不是 `True` / `False`
+
+- `auto_upload`: (int: 1 / 0)
--- dl.py
+++ dl.py
@@ -13,6 +13,7 @@ print(f'''
 [Prompt] API From: https://sex.nyan.xyz
 ''')
 
+import os
 import sys
 import time
 import requests
@@ -21,28 +22,42 @@ from PIL import Image
 
 global waittime
 global base_site
-global time_style
+global no_r18
+global max_size
 
 # Config
 
 waittime = 10 # 等待时间 (秒)
 base_site = 'https://cfpx.wyf9.top/https://sex.nyan.xyz' # Base API 站点 (末尾不带 `/`)
 # Default: 'https://sex.nyan.xyz'
-# Default: '[%Y-%m-%d %H:%M:%S]' -> "[2024-03-02 12:10:35]"
+no_r18 = True # Disable r18: True / False
+max_size = 10 # 最大文件大小(以 mb 为单位)
 
 # 检查命令行参数的数量
-if len(sys.argv) < 4:
-    print("[Tip] Usage: python3 dl.py <count> <last> <r18>")
+if len(sys.argv) < 5:
+    print("[Tip] Usage: python3 dl.py <count> <last> <r18> <auto_upload>")
     exit(1)
 else:
-    # 获取第一个命令行参数
-    count = int(sys.argv[1])
-    last = int(sys.argv[2])
-    r18 = sys.argv[3]
-    if (r18 != "true") and (r18 != "false"):
-        print("[Tip] r18: true / false")
-    print(f"[Info] count: {count} / last: {last} / r18: {r18}")
+    pass
+    
+# 获取命令行参数
+count = int(sys.argv[1])
+last = int(sys.argv[2])
+r18 = sys.argv[3]
+auto_upload = int(sys.argv[4])
+if (r18 != "true") and (r18 != "false"):
+    print("[Tip] r18: 'true' / 'false'")
+if (auto_upload != 0) and (auto_upload != 1):
+    print("[Tip] auto_upload: 0 / 1")
 
+# Check r18 disable
+if (r18 != "false") and (no_r18 == True):
+        print("[Warning] r18 Disabled. Set to 'false'")
+        r18 = 'false'
+
+print(f"[Info] count: {count} / last: {last} / r18: {r18} / auto_upload: {auto_upload}")
+
+# 检查图片
 def verifyimg(file_path):
     try:
         img = Image.open(file_path)
@@ -50,6 +65,23 @@ def verifyimg(file_path):
         return True
     except (IOError, SyntaxError) as e:
         return False
+        
+# 检查大小
+def checksize(file_path, max_size):
+    size_in_bytes = float(os.path.getsize(file_path))
+    size_in_megabytes = float(size_in_bytes / (1024 * 1024))  # 转换为MB
+    return size_in_megabytes <= max_size
+
+
+# 自动上传
+def upload():
+    print(f"[Uploading] {datetime.now().strftime('[%Y-%m-%d %H:%M:%S]')} [Auto Upload]: Run git add")
+    print(os.system('git add .'))
+    upl_time = datetime.now().strftime('[%Y-%m-%d %H:%M:%S]')
+    print(f"[Uploading] {datetime.now().strftime('[%Y-%m-%d %H:%M:%S]')} [Auto Upload]: Run git commit")
+    print(os.system(f'git commit -S -m "Auto Upload: {upl_time}"'))
+    print(f"[Uploading] {datetime.now().strftime('[%Y-%m-%d %H:%M:%S]')} [Auto Upload]: Run git push")
+    print(os.system('git push'))
 
 def get_and_save_image(url, filename):
     response = requests.get(url, stream=True)
@@ -72,7 +104,6 @@ while True:
         image_filename = f"r18img/image_{count}.jpg"
     else:
         image_filename = f"img/image_{count}.jpg"
-    #timenow = datetime.now().strftime('[%Y-%m-%d %H:%M:%S]')
     print(f"[Running] {datetime.now().strftime('[%Y-%m-%d %H:%M:%S]')} Downloading #{count} / {last}...")
     
     while True:
@@ -80,14 +111,22 @@ while True:
         
         print(f"[Running] {datetime.now().strftime('[%Y-%m-%d %H:%M:%S]')} Checking image #{count}: '{image_filename}'...")
         if verifyimg(image_filename):
-            print(f"[Running] {datetime.now().strftime('[%Y-%m-%d %H:%M:%S]')} #{count} Verify OK.")
-            break
+            print(f"[Running] {datetime.now().strftime('[%Y-%m-%d %H:%M:%S]')} #{count} Verify image OK.")
+            if checksize(image_filename, max_size):
+                print(f"[Running] {datetime.now().strftime('[%Y-%m-%d %H:%M:%S]')} #{count} Verify size OK.")
+                break
+            else:
+                print(f"[Warning] {datetime.now().strftime('[%Y-%m-%d %H:%M:%S]')} #{count} File size too large, Retrying...")
         else:
             print(f"[Warning] {datetime.now().strftime('[%Y-%m-%d %H:%M:%S]')} #{count} Invaild image, Retrying...")
     print(f"[Running] {datetime.now().strftime('[%Y-%m-%d %H:%M:%S]')} #{count} downloaded.")
 
     if count >= last:
         print(f"[Info] {datetime.now().strftime('[%Y-%m-%d %H:%M:%S]')} #{last} End.")
+        if auto_upload == 1:
+            print(f"[Uploading] {datetime.now().strftime('[%Y-%m-%d %H:%M:%S]')} Auto Upload Started.")
+            upload()
+            print(f"[Warning] {datetime.now().strftime('[%Y-%m-%d %H:%M:%S]')} Auto Upload Ended.")
         exit()
   
     # 增加编号
